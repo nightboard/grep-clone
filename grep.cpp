389b68d0a4694accb37dba6318d0441c8b66b083
visrut <visrutnavadiya111@gmail.com>
2020-11-07 22:03:30 +0530

    need to find substring find algorithm :

--- grep.cpp
+++ grep.cpp
@@ -6,20 +6,37 @@ typedef enum {
 } State;
 
 State checkValidity(int argc);
+void checkInBuffer(std::string& str);
 
 int main(int argc,char* argv[]) {
-    checkValidity(argc);
+    std::string str;
+    switch(checkValidity(argc)) {
+        case ONE_ARGUMENT:
+            str = argv[1];
+            checkInBuffer(str);
+            break;
+        case TWO_ARGUMENTS:
+
+            break;
+    }
     return 0;
 }
 
 State checkValidity(int argc) {
-    if(argc <= 0) {
+    if(argc <= 1) {
         std::cout<<"No arguments specified\n";
         std::cout<<"grep --help for more information\n";
         exit(1);
-    } else if(argc == 1) {
+    } else if(argc == 2) {
         return ONE_ARGUMENT;
     } else {
         return TWO_ARGUMENTS;
+    }
+}
+
+void checkInBuffer(std::string& str) {
+    std::string line;
+    while(true) {
+        getline(std::cin,line);
     }
 }
